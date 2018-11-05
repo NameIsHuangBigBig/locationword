f65972e157655edb0d84c6848df0ba11c8562882
YZJ <1293308455@qq.com>
2018-11-05 10:36:33 +0800

    11-5   YZJ   代码

--- app/src/main/java/com/example/locationword/locationword/MineActivity.java
+++ app/src/main/java/com/example/locationword/locationword/MineActivity.java
@@ -13,7 +13,7 @@ import android.widget.TextView;
 
 public class MineActivity extends AppCompatActivity implements View.OnClickListener{
 
-    ImageView i_head;
+    ImageView i_head,i_information,i_group;
     TextView t_name,t_phone,t_realname,t_age,t_sex;
     LinearLayout layout_realname,layout_age,layout_sex;
 
@@ -23,6 +23,8 @@ public class MineActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.mine_layout);
 
         i_head = (ImageView)findViewById(R.id.img_head);
+        i_information = (ImageView)findViewById(R.id.img_information);
+        i_group = (ImageView)findViewById(R.id.img_group);
 
         t_name = (TextView)findViewById(R.id.tv_name);
         t_phone = (TextView)findViewById(R.id.tv_phone);
@@ -48,28 +50,42 @@ public class MineActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.img_head:
                 //修改头像
                 break;
+            case R.id.img_information:
+                //跳转到聊天页
+                break;
+            case R.id.img_group:
+                //跳转到群组页
+                Intent intent = new Intent(this,ModifyNameActivity.class);
+                startActivity(intent);
+                break;
             case R.id.tv_name:
                 //修改昵称
                 Log.i("666","昵称");
-                Intent intent = new Intent(this,ModifyNameActivity.class);
-                startActivity(intent);
+                Intent intent1 = new Intent(this,ModifyNameActivity.class);
+                startActivity(intent1);
                 break;
             case R.id.tv_phone:
                 //修改电话号码
                 Log.i("666","号码");
+                Intent intent2 = new Intent(this,ModifyPhoneActivity.class);
+                startActivity(intent2);
                 break;
             case R.id.realname_layout:
                 //修改真实姓名
                 Log.i("666","真实姓名");
+                Intent intent3 = new Intent(this,ModifyRealnameActivity.class);
+                startActivity(intent3);
                 break;
             case R.id.age_layout:
                 //修改年龄
                 Log.i("666","年龄");
+                Intent intent4 = new Intent(this,ModifyAgeActivity.class);
+                startActivity(intent4);
                 break;
             case R.id.sex_layout:
                 //修改性别
                 Log.i("666","性别");
-                new AlertDialog.Builder(this).setTitle("请选择性别").setIcon(R.mipmap.ic_launcher).setSingleChoiceItems(new String[]{"男","女"},0,new DialogInterface.OnClickListener(){public void onClick(DialogInterface dialogInterface, int which){
+                new AlertDialog.Builder(this).setTitle("请选择性别").setIcon(R.mipmap.ic_launcher).setSingleChoiceItems(new String[]{"男","女"},0,new DialogInterface.OnClickListener(){public void onClick(DialogInterface dialogInterface,int which){
                 }}).setPositiveButton("确定",null).show();
                 break;
         }
